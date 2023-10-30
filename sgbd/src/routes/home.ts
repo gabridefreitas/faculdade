30a15fc5a805bc3b7f8a0d6b6b420f71fe26a846
Gabriel <contato.gabrieldefreitas@gmail.com>
2023-10-30 13:36:42 -0300

    refactor: :fire: move sgbd to its own repo

--- sgbd/src/routes/home.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/", async (_req, res) => {
-  res.json({ message: "SGBD - Atividade 8" }).status(200);
-});
-
-export default router;
